045ed3e3ca4d64b10c4a9db47542f0dffce68bfe
Christopher Biscardi <biscarch@sketcht.com>
2018-11-07 01:09:37 -0800

    [tokens]: make each color 11 values

--- ui-surfaces/sens8-www/src/docs/tokens/core/color-boxes.js
+++ ui-surfaces/sens8-www/src/docs/tokens/core/color-boxes.js
@@ -6,32 +6,13 @@ export default () => (
   <div
     css={`
       display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-gap: 1rem;
+      grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+      grid-gap: 2rem;
     `}
   >
-    {Object.entries(theme.colors.raw).map(
-      ([k, v]) =>
-        typeof v === "string" ? (
-          <SingleColorBox title={k} color={v} />
-        ) : (
-          <MultiColorBox title={k} colors={v} />
-        )
-    )}
-  </div>
-);
-
-const SingleColorBox = ({ title, color }) => (
-  <div>
-    <label>{title}</label>
-    <span
-      css={`
-        display: flex;
-        background: ${color};
-        height: 10rem;
-        width: 10rem;
-      `}
-    />
+    {Object.entries(theme.colors.raw).map(([k, v]) => (
+      <MultiColorBox title={k} colors={v} />
+    ))}
   </div>
 );
 
